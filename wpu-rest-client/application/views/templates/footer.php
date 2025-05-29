32a4fb13402dee82d29389de48b1037de80dd407
fadilanur28 <fadlinurhutapea@gmail.com>
2025-05-29 17:51:33 +0700

    memperbarui isi dari footer pada wpu-rest client

--- wpu-rest-client/application/views/templates/footer.php
+++ wpu-rest-client/application/views/templates/footer.php
@@ -1,20 +1,40 @@
-     <!-- footer -->
-    <footer class="bg-dark text-white mt-5">
-      <div class="container">
-        <div class="row">
-          <div class="col text-center">
-            <p>Copyright &copy; 2025.</p>
-          </div>
-        </div>
-      </div>
-    </footer>
- 
- <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/sweetalert2.all.min.js"></script>
-    <script src="<?= base_url(); ?>assets/js/myscript.js"></script>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Halaman Contoh</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+    }
+    main {
+      flex: 1;
+    }
+  </style>
+</head>
+<body>
+
+  <!-- Konten utama halaman -->
+  <main class="container my-5">
+    <h1></h1>
+    <p>.</p>
+  </main>
+
+ <!-- Footer -->
+<footer class="bg-dark text-white text-center py-3" style="width: 100%;">
+  <p class="mb-0">&copy; <?= date('Y'); ?>. All rights reserved.</p>
+</footer>
+
+
+
+  <!-- Script JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="<?= base_url(); ?>assets/js/sweetalert2.all.min.js"></script>
+  <script src="<?= base_url(); ?>assets/js/myscript.js"></script>
+</body>
 </html>
